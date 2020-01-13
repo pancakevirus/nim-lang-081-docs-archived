6acefa7e4e7417646261b854c5141f7072b0abb6
megumi_engines <megumi_engines@users.osdn.me>
2020-01-13 16:28:55 +0900

    ・日本語版ウェブサイト原稿の追加 (Jekyll) ・OmegaT/html版で重複ファイルがあるのを削除 (誤って source フォルダの中身を grossaryに複写していました)
    注意
    ・tut1.html の引用句が未だ仮訳です。引用はお控えください。
    ・現行作業対象は Nim 1.04 です。最新版が公開されましたら、なるべく追従していく予定です。

---
+++ website/_site/run.bat
@@ -0,0 +1,4 @@
+
+rem path=%path%;"C:\Program Files (x86)\Sphinx\bin";"C:\Program Files (x86)\Sphinx\python";
+
+bundle exec jekyll serve --incremental
---
+++ website/mkjunction.bat
@@ -0,0 +1,2 @@
+set nimdocver=1.0.2
+mklink /J c:\website-master\jekyll\_site\docs c:\nim\web\upload\%nimdocver%
